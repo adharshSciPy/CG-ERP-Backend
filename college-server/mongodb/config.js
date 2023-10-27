2a125d339e71c64bfd4ac2b916bfb26ba7435ca9
Sruthi <sruthiscipy@gmail.com>
2023-10-27 02:45:31 -0700

    teacher route

--- college-server/mongodb/config.js
+++ college-server/mongodb/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 async function connect() {
     // mongodb connection
     mongoose.set('strictQuery', false)
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(process.env.MONGO_URL)
         .then(() => {
             console.log('Mongodb Connected')
         })
